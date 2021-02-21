6a02e869789fe73252472f0b2bfb4c28c81987ff
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-21 18:42:29 +0700

    Checkbox And RadioButton

--- Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
+++ Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.philipplacknercourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.Toast
 import com.example.philipplacknercourse.databinding.ActivityMainBinding
 
@@ -13,9 +14,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnAdd.setOnClickListener {
-            binding.Ivanime.setImageResource(R.drawable.anim_satoru_gojo)
-            Toast.makeText(applicationContext, "Satoru Gojo", Toast.LENGTH_SHORT).show()
+        binding.btnOrder.setOnClickListener {
+            val checkedMealRadioButtonId = binding.rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMealRadioButtonId)
+            val cheese = binding.cbCheese.isChecked
+            val onions = binding.cbOnion.isChecked
+            val salad = binding.cbSalad.isChecked
+            val orderString = "You ordered a burger with : \n" +
+                    "${meat.text}" +
+                    (if (cheese) "\nCheese" else "") +
+                    (if (onions) "\nOnions" else "") +
+                    (if (salad) "\nSalad" else "")
+            binding.tvOrder.text = orderString
+
         }
 
     }
