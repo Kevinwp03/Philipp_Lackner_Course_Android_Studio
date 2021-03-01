d8ed9bc6046311905533fed6feff0f4fac20e2f2
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-01 15:53:22 +0700

    Alert Dialog

--- Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
+++ Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
@@ -4,19 +4,74 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //AlertDialog
+        val addContactDialog = AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_add_contact)
+            .setTitle("Add Contact")
+            .setMessage("Do you want to Add Ms. Dian to your contact list?")
+            .setPositiveButton("YES"){ _,_ ->  // " _ " : artinya tidak menjalankan program tertentu
+                Toast.makeText(this, "You added Ms. Dian to Your Contact", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("NO"){ _,_ ->
+                Toast.makeText(this, "You didn't add Ms. Dian to Your Contact", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog1.setOnClickListener {
+            addContactDialog.show()
+        }
+
+        // Alert Dialog Untuk beberapa Pilihan (Single Choice Option)
+        val option = arrayOf("First Option", "Second Option", "Third Option")
+        val singelChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of these option")
+            .setSingleChoiceItems(option, 0) { _, i ->
+                Toast.makeText(this, "You Click On ${option[i]}", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("YES"){ _,_ ->  // " _ " : artinya tidak menjalankan program tertentu
+                Toast.makeText(this, "You accept the Single Choice Dialog", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("NO"){ _,_ ->
+                Toast.makeText(this, "You declined the Single Choice Dialog", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog2.setOnClickListener {
+            singelChoiceDialog.show()
+        }
+
+        val multiplyChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of these option")
+            .setMultiChoiceItems(option, booleanArrayOf(false, false, false)) {_, i, isChecked ->
+                if (isChecked){
+                Toast.makeText(this, "You Checked ${option[i]}", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "You Uncheked ${option[i]}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setPositiveButton("YES"){ _,_ ->  // " _ " : artinya tidak menjalankan program tersebut
+                Toast.makeText(this, "You accept the Multiply Choice Dialog", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("NO"){ _,_ ->
+                Toast.makeText(this, "You declined the Multiply Choice Dialog", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog3.setOnClickListener {
+            multiplyChoiceDialog.show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.app_bar_menu, menu)
         return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.miAddContact -> Toast.makeText(this, "You Click on Add Contact", Toast.LENGTH_SHORT).show()
@@ -27,4 +82,5 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
+
 }
