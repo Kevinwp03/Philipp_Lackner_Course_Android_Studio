3ca9ea963fbe680ee16c8a3b8475b0493bf289ef
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-02 23:10:50 +0700

    Bottom Navigation View

--- Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
+++ Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.philipplacknercourse
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,26 +12,29 @@ class MainActivity : AppCompatActivity() {
 
         val firstFragment = FirstFragment()
         val secondFragment = SecondFragment()
+        val thirdFragment = ThirdFragment()
 
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(firstFragment)
+                R.id.miMessage -> setCurrentFragment(secondFragment)
+                R.id.miProfile -> setCurrentFragment(thirdFragment)
+            }
+            true //Lamda
+        }
+
+        bottomNavigationView.getOrCreateBadge(R.id.miMessage).apply {
+            number = 10
+            isVisible = true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, firstFragment)
-            addToBackStack(null)
-            commit()
+            replace(R.id.flFragment, fragment)
+            commit() //jangan lupa Commit
         }
-
-        btnFragment1.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment, firstFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
-        btnFragment2.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment, secondFragment)
-                commit()
-            }
-        }
-
     }
 }
--- Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/SecondFragment.kt
+++ Philipp Lackner Cource Android Studio/app/src/main/java/com/example/philipplacknercourse/SecondFragment.kt
@@ -2,5 +2,5 @@ package com.example.philipplacknercourse
 
 import androidx.fragment.app.Fragment
 
-class SecondFragment : Fragment(R.layout.fragment_second) {
+class SecondFragment : Fragment(R.layout.fragment_second){
 }
